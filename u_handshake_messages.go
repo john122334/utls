54dc884b9981c63dd834e0d81cca1cb8f3883f34
Bogdan Finn <bogdan.finn@googlemail.com>
2022-11-20 00:29:33 +0100

    rebase on upstream utls;

--- u_handshake_messages.go
+++ u_handshake_messages.go
@@ -62,7 +62,7 @@ type utlsEncryptedExtensionsMsgExtraFields struct {
 
 func (m *encryptedExtensionsMsg) utlsUnmarshal(extension uint16, extData cryptobyte.String) bool {
 	switch extension {
-	case utlsExtensionApplicationSettings:
+	case ExtensionALPS:
 		m.utls.hasApplicationSettings = true
 		m.utls.applicationSettings = []byte(extData)
 	case utlsExtensionECH:
@@ -92,7 +92,7 @@ func (m *utlsClientEncryptedExtensionsMsg) marshal() (x []byte, err error) {
 	builder.AddUint24LengthPrefixed(func(body *cryptobyte.Builder) {
 		body.AddUint16LengthPrefixed(func(extensions *cryptobyte.Builder) {
 			if m.hasApplicationSettings {
-				extensions.AddUint16(utlsExtensionApplicationSettings)
+				extensions.AddUint16(ExtensionALPS)
 				extensions.AddUint16LengthPrefixed(func(msg *cryptobyte.Builder) {
 					msg.AddBytes(m.applicationSettings)
 				})
@@ -129,7 +129,7 @@ func (m *utlsClientEncryptedExtensionsMsg) unmarshal(data []byte) bool {
 		}
 
 		switch extension {
-		case utlsExtensionApplicationSettings:
+		case ExtensionALPS:
 			m.hasApplicationSettings = true
 			m.applicationSettings = []byte(extData)
 		default:
